451484512cfd7fedcd2b0403468970439718b0d9
colbyderango <colbyderango@gmail.com>
2017-04-02 14:06:11 -0600

    deploy

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import io from 'socket.io-client'
+import axios from 'axios'
 import store from './store'
 
 Vue.config.productionTip = false
